071ec6f10b5c3da0e0d9e5f95a14265b5f8b9b0e
Cevher Geşgin <61330830+cgesgin@users.noreply.github.com>
2020-12-21 21:36:33 +0300

    Update InfixToPostfix.java

--- Veri_Yapilari_Lab_Hafta11_ödev/src/hafta11/BinaryTree/InfixToPostfix.java
+++ Veri_Yapilari_Lab_Hafta11_ödev/src/hafta11/BinaryTree/InfixToPostfix.java
@@ -2,79 +2,55 @@ package hafta11.BinaryTree;
 
 public class InfixToPostfix {
 
-    private String input;
-    private String output;
-    private Stack stack;
+    String output;
+    String input;
 
     public InfixToPostfix(String input) {
-        this.input = input;
-        this.output = "";
-        this.stack = new Stack(this.input.length());
+        this.input=input;
+        output="";
+    }
+    
+    public int op(char ch) {
+        switch (ch) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+        }
+        return -1;
     }
 
     public String doPostfix() {
-        char ch;
-        for (int i = 0; i < this.input.length(); i++) {
-            ch = this.input.charAt(i);
-            switch (ch) {
-                case '+':
-                case '-':
-                    this.gotOper(ch, 1);
-                    break;
-                case '*':
-                case '/':
-                    this.gotOper(ch, 1);
-                    break;
-                case '(':
-                    this.stack.pushChar(ch);
-                    break;
-                case ')':
-                    this.gotParen(ch);
-                    break;
-                default:
-                    this.output += ch;
-                    break;
+        Stack stack=new Stack(input.length());
+        for (int i = 0; i < input.length(); ++i) {
+            char c = input.charAt(i);
+            if (Character.isLetterOrDigit(c)) {
+                output += c;
+            } else if (c == '(') {
+                stack.pushChar(c);
+            } else if (c == ')') {
+                while (!stack.isEmptyChar()
+                        && stack.peekChar() != '(') {
+                    output += stack.popChar();
+                }
+                stack.pop();
+            } else {
+                while (!stack.isEmptyChar() && op(c)
+                        <= op(stack.peekChar())) {
+                    output += stack.pop();
+                }
+                stack.pushChar(c);
             }
         }
-        while (!stack.isEmpty()) {
-            output = output + stack.popChar();
+        while (!stack.isEmptyChar()) {
+            if (stack.peekChar() == '(') {
+                return "Eksik parantez";
+            }
+            output += stack.pop();
         }
         return output;
     }
 
-    public void gotOper(char opThis, int a1) {
-        while (!this.stack.isEmpty()) {
-            char operatorTop = this.stack.popChar();
-            if (operatorTop == '(') {
-                this.stack.pushChar(operatorTop);
-                break;
-            } else {
-                int a2;
-                if (operatorTop == '+' || operatorTop == '-') {
-                    a2 = 1;
-                } else {
-                    a2 = 2;
-                }
-                if (a2 < a1) {
-                    stack.pushChar(operatorTop);
-                    break;
-                } else {
-                    output = output + operatorTop;
-                }
-            }
-        }
-        stack.pushChar(opThis);
-    }
-
-    public void gotParen(char ch) {
-        while (!this.stack.isEmpty()) {
-            char ch2 = this.stack.popChar();
-            if (ch2 == '(') {
-                break;
-            } else {
-                this.output += ch2;
-            }
-        }
-    }
 }
-
